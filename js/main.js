8c24baa222eda1dfd0296424a56239f5d73db93c
Vitaly Blokhin <karambafe@yahoo.com>
2016-09-26 23:53:23 +0300

    add navbar

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function getCoords(elem) { // кроме IE8-
+  var box = elem.getBoundingClientRect();
+  return {
+    top: box.top + pageYOffset
+  };
+}
+
+var navbar = document.getElementsByClassName('navbar')[0];
+var forNavbarItem = document.getElementsByClassName('fornavbar')[0];
+var navbarCoords = getCoords(forNavbarItem);
+
+window.onscroll = function() {
+  if (navbarCoords.top < pageYOffset + 50 ) {
+    navbar.classList.add('navbar_fixied');
+  } else {
+    navbar.classList.remove('navbar_fixied');
+  }
+};
