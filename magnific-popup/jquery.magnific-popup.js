3be07dc1903fbf9724a15c254ae20e40f0e80194
Vitaly Blokhin <karambafe@yahoo.com>
2016-09-30 14:51:27 +0300

    remove remodal and title, img source for madginific

--- magnific-popup/jquery.magnific-popup.js
+++ magnific-popup/jquery.magnific-popup.js
@@ -1135,8 +1135,8 @@ $.magnificPopup.registerModule('image', {
 						'<div class="mfp-img"></div>'+
 						'<figcaption>'+
 							'<div class="mfp-bottom-bar">'+
-								'<div class="mfp-title"></div>'+
-								'<div class="mfp-counter"></div>'+
+								// '<div class="mfp-title"></div>'+
+								// '<div class="mfp-counter"></div>'+
 							'</div>'+
 						'</figcaption>'+
 					'</figure>'+
