f39f50b4ded94305f07c7806f6b525d66870289b
Vitaly Blokhin <karambafe@yahoo.com>
2016-09-30 20:04:37 +0300

    change background

--- corrosion.php
+++ corrosion.php
@@ -9,7 +9,9 @@ $type = htmlspecialchars($_POST["type"]);
 		$fromMail = 'Interplast@yandex.ru';
 		$fromName = 'Interplast';
 /* Сюда впишите свою эл. почту */
-$address = "karambafe@yahoo.com";
+$address = "vakulenko@interplastn.ru" . ", ";
+$address .= "subaru@interplastn.ru" . ", ";
+$address .= "info@upfly.ru";
 $headers = "Content-type: text/plain; charset=\"utf-8\"\r\n";
 $headers .= "From: ". $fromName ." <". $fromMail ."> \r\n";
 /* А здесь прописывается текст сообщения, \n - перенос строки */
